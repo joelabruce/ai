86fcd8b2caf15dbf7d679fdc470b4a74bab6ae0b
LAPTOP-OEHQIHDL\joela <yoale.bruce@gmail.com>
2024-12-30 05:25:56 -0800

    Implement iterator for generating identity matrices given a size of n

--- src/geoalg/f32_math.rs
+++ src/geoalg/f32_math.rs
@@ -1,5 +1,7 @@
 pub mod basis_vectors;
 pub mod bivectors;
 pub mod complex;
+pub mod matrix;
 pub mod vec2f;
 pub mod vec3f;
+pub mod vecnf;
---
+++ src/geoalg/f32_math/convolution_kernel.rs
@@ -0,0 +1,7 @@
+struct ConvolutionKernel {
+    i: u32,
+    j: u32,
+
+    data: Vec<f32>
+}
+
