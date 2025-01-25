5e6fdc01e85f005b1ed3ea41d91ae31fe987903f
LAPTOP-OEHQIHDL\joela <yoale.bruce@gmail.com>
2025-01-25 13:11:16 -0800

    Cleanup Sample code. Begin max pooling layer implementation

--- src/statistics/sample.rs
+++ src/statistics/sample.rs
@@ -3,47 +3,51 @@ use rand::Rng;
 pub struct Sample<T> {
     all_samples: Vec<T>,
     unused: Vec<usize>,
-    consumed: Vec<usize>,
+    used: Vec<usize>,
 }
 
 impl<T> Sample<T> {
     /// Creates a sample of data from specified vector.
+    /// Make sure to move the data into sample for it to be consumed.
+    /// Requires no expensive shuffling since the data load is likely already expensive.
     pub fn create_sample(data: Vec<T>) -> Sample<T> {
-        let unused = data.iter().enumerate().map(|(i, _)| i).collect();
+        let unused = (0..data.len()).collect();
+        let used = Vec::with_capacity(data.len());
         let all_samples = data;
-        let consumed = Vec::with_capacity(all_samples.len());
 
         Sample {
             all_samples,
             unused,
-            consumed
+            used
         }
     }
 
     /// Creates a random batch from sample data.
+    /// Repeated calls to random_batch are guaranteed to never uses the same element more than once before call to reset.
+    /// Must call reset once the sample is exhausted to use in a new series of batches.
+    /// Data is owned by sample, and should only ever be immutably borrowed.
     pub fn random_batch(&mut self, requested_batch_size: usize) -> Vec<&T> {
         // Ensure batch size picks up stragglers
         let batch_size = std::cmp::min(requested_batch_size, self.unused.len());
 
         let mut rng = rand::thread_rng();
-
         let mut newbatch = Vec::with_capacity(batch_size);
         for _ in 0..batch_size {
-            let index = rng.gen_range(0..self.unused.len());
-            let img_index = self.unused.remove(index);
-            self.consumed.push(img_index);
+            let unused_index = rng.gen_range(0..self.unused.len());
+            let index_to_use = self.unused.remove(unused_index);
+            self.used.push(index_to_use);
 
-            let img = &self.all_samples[img_index];
-            newbatch.push(img);
+            let data_reference = &self.all_samples[index_to_use];
+            newbatch.push(data_reference);
         }
 
         newbatch
     }
 
-    /// Resets batch.
+    /// Resets batch sample to allow a fresh series of random batches.
     pub fn reset(&mut self) {
-        self.unused.extend(self.consumed.iter());
-        self.consumed = Vec::with_capacity(self.unused.len());
+        self.unused.extend(self.used.iter());
+        self.used.clear();
     }
 }
 
@@ -54,20 +58,29 @@ mod tests {
     #[test]
     fn test_batch() {
         let mut sample = Sample::create_sample( 
-            vec!["a", "b", "c", "d", "e", "f", "g"]
+            vec!["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
         );
 
-        for _ in 0..4 {
-            let _new_batch = sample.random_batch(2);
-            
-            //println!("batch: {:?}", _new_batch);
+        for reset in 0..5 {
+            println!("TC1 Reset: {reset}");
+            for _ in 0..6 {
+                let _new_batch = sample.random_batch(2);
+                
+                println!("sample: {:?}", _new_batch);
+            }
+
+            sample.reset();
         }
 
-        sample.reset();
-        for _ in 0..3 {
-            let _new_batch = sample.random_batch(3);
-            
-            //println!("batch: {:?}", _new_batch);
+        for reset in 0..4 {
+            println!("TC2 Reset: {reset}");
+            for _ in 0..4 {
+                let _new_batch = sample.random_batch(3);
+                
+                println!("sample: {:?}", _new_batch);
+            }
+
+            sample.reset();
         }
     }
 }
